e0a53da718182b4bfe2a4f9a269c2133d20ef1a0
pn <517675667@qq.com>
2020-05-17 18:11:36 +0800

    加入了RRT

--- src/grid_path_searcher/src/demo_node.cpp
+++ src/grid_path_searcher/src/demo_node.cpp
@@ -15,6 +15,18 @@
 #include "Astar_searcher.h"
 #include "JPS_searcher.h"
 #include "backward.hpp"
+
+#include "graph_searcher.h"
+#include <ompl/config.h>
+#include <ompl/base/StateSpace.h>
+#include <ompl/base/Path.h>
+#include <ompl/base/spaces/RealVectorBounds.h>
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+#include <ompl/base/StateValidityChecker.h>
+#include <ompl/base/OptimizationObjective.h>
+#include <ompl/base/objectives/PathLengthOptimizationObjective.h>
+#include <ompl/geometric/planners/rrt/RRTstar.h>
+#include <ompl/geometric/SimpleSetup.h>
 //#include <waypoint_trajectory_generator/trajpoint.h>
 //#include <waypoint_trajectory_generator/Trajectoy.h>
 // #include <waypoint_trajectory_generator/trajpoint.h>
@@ -25,6 +37,11 @@ using namespace Eigen;
 namespace backward {
 backward::SignalHandling sh;
 }
+
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
+
+#define use_RRT 1
 
 // simulation param from launch file
 double _resolution, _inv_resolution, _cloud_margin;
@@ -39,15 +56,142 @@ int _max_x_id, _max_y_id, _max_z_id;
 
 // ros related
 ros::Subscriber _map_sub, _pts_sub,_traj_sub,drone_pos_sub;
-ros::Publisher  _grid_path_vis_pub,_grid_path_vis_pub_jps, _visited_nodes_vis_pub,_visited_nodes_jps_vis_pub, _grid_map_vis_pub,_simplified_waypoints_pub;
+ros::Publisher  _grid_path_vis_pub,_grid_path_vis_pub_jps, _visited_nodes_vis_pub,
+_visited_nodes_jps_vis_pub, _grid_map_vis_pub,_simplified_waypoints_pub,_RRTstar_path_vis_pub;
 AstarPathFinder * _astar_path_finder     = new AstarPathFinder();
 JPSPathFinder   * _jps_path_finder       = new JPSPathFinder();
+
+RRTstarPreparatory * _RRTstar_preparatory     = new RRTstarPreparatory();
+
 void rcvWaypointsCallback(const nav_msgs::Path & wp);
 void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map);
 
 void visGridPath( vector<Vector3d> nodes, bool is_use_jps );
 void visVisitedNode( vector<Vector3d> nodes,bool flag);
 void pathFinding(const Vector3d start_pt, const Vector3d target_pt);
+
+void pathFinding_RRT(const Vector3d start_pt, const Vector3d target_pt);
+void visRRTstarPath(vector<Vector3d> nodes );
+
+
+// Our collision checker. For this demo, our robot's state space
+class ValidityChecker : public ob::StateValidityChecker
+{
+public:
+    ValidityChecker(const ob::SpaceInformationPtr& si) :
+        ob::StateValidityChecker(si) {}
+    // Returns whether the given state's position overlaps the
+    // circular obstacle
+    bool isValid(const ob::State* state) const
+    {   
+        // We know we're working with a RealVectorStateSpace in this
+        // example, so we downcast state into the specific type.
+        const ob::RealVectorStateSpace::StateType* state3D =
+            state->as<ob::RealVectorStateSpace::StateType>();
+        /**
+        *
+        *
+        STEP 1: Extract the robot's (x,y,z) position from its state
+        *
+        *
+        */
+        double x = state3D->values[0];
+        double y = state3D->values[1];
+        double z = state3D->values[2];
+        return _RRTstar_preparatory->isObsFree(x, y, z);
+    }
+};
+
+// Returns a structure representing the optimization objective to use
+// for optimal motion planning. This method returns an objective which
+// attempts to minimize the length in configuration space of computed
+// paths.
+ob::OptimizationObjectivePtr getPathLengthObjective(const ob::SpaceInformationPtr& si)
+{
+    return ob::OptimizationObjectivePtr(new ob::PathLengthOptimizationObjective(si));
+}
+
+ob::OptimizationObjectivePtr getThresholdPathLengthObj(const ob::SpaceInformationPtr& si)
+{
+    ob::OptimizationObjectivePtr obj(new ob::PathLengthOptimizationObjective(si));
+    obj->setCostThreshold(ob::Cost(1.51));
+    return obj;
+}
+
+void pathFinding_RRT(const Vector3d start_pt, const Vector3d target_pt)
+{
+    // Construct the robot state space in which we're planning. 
+    ob::StateSpacePtr space(new ob::RealVectorStateSpace(3));
+
+    // Set the bounds of space to be in [0,1].
+    ob::RealVectorBounds bounds(3);
+    bounds.setLow(0, - _x_size * 0.5);
+    bounds.setLow(1, - _y_size * 0.5);
+    bounds.setLow(2, 0.0);
+
+    bounds.setHigh(0, + _x_size * 0.5);
+    bounds.setHigh(1, + _y_size * 0.5);
+    bounds.setHigh(2, _z_size);
+
+    space->as<ob::RealVectorStateSpace>()->setBounds(bounds);
+    // Construct a space information instance for this state space
+    ob::SpaceInformationPtr si(new ob::SpaceInformation(space));
+    // Set the object used to check which states in the space are valid
+    si->setStateValidityChecker(ob::StateValidityCheckerPtr(new ValidityChecker(si)));
+    si->setup();
+
+    // Set our robot's starting state
+    ob::ScopedState<> start(space);
+    start[0] = start_pt(0);
+    start[1] = start_pt(1);
+    start[2] = start_pt(2);
+
+    // Set our robot's goal state
+    ob::ScopedState<> goal(space);
+
+
+    goal[0] = target_pt(0);
+    goal[1] = target_pt(1);
+    goal[2] = target_pt(2);    
+
+
+   auto pdef(std::make_shared<ob::ProblemDefinition>(si));
+    // Set the start and goal states
+    pdef->setStartAndGoalStates(start, goal);
+
+    pdef->setOptimizationObjective(getPathLengthObjective(si));
+    // Set the problem instance for our planner to solve
+    ob::PlannerPtr optimizingPlanner(new og::RRTstar(si));
+    optimizingPlanner->setProblemDefinition(pdef);
+    optimizingPlanner->setup();
+
+    // attempt to solve the planning problem within one second of
+    // planning time
+    ob::PlannerStatus solved = optimizingPlanner->solve(1.0);
+
+    if (solved)
+    {
+        // get the goal representation from the problem definition (not the same as the goal state)
+        // and inquire about the found path
+        og::PathGeometric* path = pdef->getSolutionPath()->as<og::PathGeometric>();
+        
+        vector<Vector3d> path_points;
+
+        for (size_t path_idx = 0; path_idx < path->getStateCount (); path_idx++)
+        {
+            const ob::RealVectorStateSpace::StateType *state = path->getState(path_idx)->as<ob::RealVectorStateSpace::StateType>(); 
+            Vector3d point;
+            point(0) = (*state)[0];
+            point(1) = (*state)[1];
+            point(2) = (*state)[2];
+            path_points.push_back(point);            
+
+        }
+        visGridPath(path_points,1);  
+        // visRRTstarPath(path_points);     
+    }
+}
+
 
 void rcvWaypointsCallback(const nav_msgs::Path & wp)
 {     
@@ -60,7 +204,10 @@ void rcvWaypointsCallback(const nav_msgs::Path & wp)
                  wp.poses[0].pose.position.z;
 
     ROS_INFO("[node] receive the planning target");
-    pathFinding(_start_pt, target_pt); 
+    if (!use_RRT)
+        pathFinding(_start_pt, target_pt); 
+    else
+        pathFinding_RRT(_start_pt, target_pt); 
 }
 
 void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map)
@@ -85,6 +232,7 @@ void rcvPointCloudCallBack(const sensor_msgs::PointCloud2 & pointcloud_map)
         // set obstalces into grid map for path planning
         _astar_path_finder->setObs(pt.x, pt.y, pt.z);
         _jps_path_finder->setObs(pt.x, pt.y, pt.z);
+        _RRTstar_preparatory->setObs(pt.x, pt.y, pt.z);
 
         // for visualize only
         Vector3d cor_round = _astar_path_finder->coordRounding(Vector3d(pt.x, pt.y, pt.z));
@@ -194,6 +342,8 @@ int main(int argc, char** argv)
     _visited_nodes_jps_vis_pub        = nh.advertise<visualization_msgs::Marker>("visited_nodes_jps_vis", 1);
     _simplified_waypoints_pub     = nh.advertise<nav_msgs::Path>("simplified_waypoints",50);//发布优化后的轨迹
     
+    _RRTstar_path_vis_pub         = nh.advertise<visualization_msgs::Marker>("RRTstar_path_vis",1);
+
     nh.param("map/cloud_margin",  _cloud_margin, 0.0);
     nh.param("map/resolution",    _resolution,   0.2);
     
@@ -220,7 +370,9 @@ int main(int argc, char** argv)
     _jps_path_finder    = new JPSPathFinder();
     _jps_path_finder    -> initGridMap(_resolution, _map_lower, _map_upper, _max_x_id, _max_y_id, _max_z_id);
     
-
+    _RRTstar_preparatory  = new RRTstarPreparatory();
+    _RRTstar_preparatory  -> initGridMap(_resolution, _map_lower, _map_upper, _max_x_id, _max_y_id, _max_z_id);
+    
     ros::Rate rate(100);
     bool status = ros::ok();
 
@@ -346,4 +498,42 @@ void visVisitedNode(vector<Vector3d> nodes,bool is_use_jps)
         _visited_nodes_jps_vis_pub.publish(node_vis);
     else
         _visited_nodes_vis_pub.publish(node_vis);
+}
+
+void visRRTstarPath(vector<Vector3d> nodes )
+{
+    visualization_msgs::Marker Points, Line; 
+    Points.header.frame_id = Line.header.frame_id = "world";
+    Points.header.stamp    = Line.header.stamp    = ros::Time::now();
+    Points.ns              = Line.ns              = "demo_node/RRTstarPath";
+    Points.action          = Line.action          = visualization_msgs::Marker::ADD;
+    Points.pose.orientation.w = Line.pose.orientation.w = 1.0;
+    Points.id = 0;
+    Line.id   = 1;
+    Points.type = visualization_msgs::Marker::POINTS;
+    Line.type   = visualization_msgs::Marker::LINE_STRIP;
+
+    Points.scale.x = _resolution/2; 
+    Points.scale.y = _resolution/2;
+    Line.scale.x   = _resolution/2;
+
+    //points are green and Line Strip is blue
+    Points.color.g = 1.0f;
+    Points.color.a = 1.0;
+    Line.color.b   = 1.0;
+    Line.color.a   = 1.0;
+
+    geometry_msgs::Point pt;
+    for(int i = 0; i < int(nodes.size()); i++)
+    {
+        Vector3d coord = nodes[i];
+        pt.x = coord(0);
+        pt.y = coord(1);
+        pt.z = coord(2);
+
+        Points.points.push_back(pt);
+        Line.points.push_back(pt);
+    }
+    _RRTstar_path_vis_pub.publish(Points);
+    _RRTstar_path_vis_pub.publish(Line); 
 }
